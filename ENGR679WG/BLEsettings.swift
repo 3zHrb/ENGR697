4a6492e385598d1d15ceb6beaf49b767d6938314
Abdulaziz Alharbi <abdulazizalharbi@AbdulazizsAir5.T-mobile.com>
2018-11-26 02:03:36 -0800

    blurtooth is working btween both viewcontrollers, and disconnect button is working on BLESettings

--- ENGR679WG/BLEsettings.swift
+++ ENGR679WG/BLEsettings.swift
@@ -9,31 +9,70 @@
 import UIKit
 import CoreBluetooth
 
+
 class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource, CBCentralManagerDelegate {
     
     
-   
-    
-
-    
-
-    var CBManager : CBCentralManager?
+    var manager : CBCentralManager? = nil//changed
     var names:[String] = []
-    var BLEDevices: [CBPeripheral] = []
+    var peripherals: [CBPeripheral] = []//changed
     var myPeripheral : CBPeripheral?
+    var parentView:MainPageViewController? = nil//added
+    var choosen : CBPeripheral? = nil
     
     var uuid: Any?
+    
+    @IBOutlet weak var disconnectButton: UIBarButtonItem!
+    
+    
+    @IBAction func disconnectButton(_ sender: Any) {
+        
+        
+        if let hasChoosen = choosen{
+        manager?.cancelPeripheralConnection(hasChoosen)
+            
+            let alertVC = UIAlertController(title: "Disconnected!", message: "The device you selected is now Disconnected", preferredStyle: .alert)
+            
+            let alertAc = UIAlertAction(title: "ok", style: .default) { (UIAlertAction) in
+                
+               /* if let navController = self.navigationController {
+                    navController.popViewController(animated: true)
+                }*/
+                
+            }
+            
+            alertVC.addAction(alertAc)
+            
+            present(alertVC, animated: true, completion: nil)
+            disconnectButton.tintColor = UIColor.gray
+            disconnectButton.isEnabled = false
+        }
+    }
+    
+    
+
+  
     
     @IBOutlet weak var UITableView0: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         UITableView0.delegate = self
         UITableView0.dataSource = self
         
-        CBManager = CBCentralManager(delegate: self, queue: nil)
+        manager = CBCentralManager(delegate: self, queue: nil)
         
         //CBManager.scanForPeripherals(withServices: nil, options: nil)
+        
+        
+        if (choosen == nil){
+            
+            disconnectButton.tintColor = UIColor.gray
+            disconnectButton.isEnabled = false
+            
+            
+        }
          UITableView0.reloadData()
 
         // Do any additional setup after loading the view.
@@ -41,19 +80,22 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
     
     override func viewDidAppear(_ animated: Bool) {
         
-        CBManager?.scanForPeripherals(withServices: [CBUUID.init(string: "DFB0")], options: nil)
+        //manager?.scanForPeripherals(withServices: [CBUUID.init(string: "DFB0")], options: nil)
+        
+       
         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+       // return names.count
+        return peripherals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let CellVar = tableView.dequeueReusableCell(withIdentifier: "TheCell0", for: indexPath) as? CellTableViewCell{
             // Array(Set(names))
-            myPeripheral = BLEDevices[indexPath.row]
+           myPeripheral = peripherals[indexPath.row]
             CellVar.bluetoothName.text = myPeripheral?.name
             
             //Cellvar.uuidLable.texy =
@@ -66,20 +108,37 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let choosen = BLEDevices[indexPath.row]
-        CBManager?.connect(choosen, options: nil)
+        choosen = peripherals[indexPath.row]
+        
+        if let connected = choosen{
+        manager?.connect(connected, options: nil)
+            disconnectButton.tintColor = UIColor.red
+            disconnectButton.isEnabled = true
+        }
     }
     
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         
+        parentView?.mainPeripheral = peripheral
+        peripheral.delegate = parentView
+        peripheral.discoverServices(nil)
         
+        //set the manager's delegate view to parent so it can call relevant disconnect methods
+        manager?.delegate = parentView
+       // parentView?.customiseNavigationBar()
+        
+       /* if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }*/
+        
+        print("Connected to " +  peripheral.name!)
         let alertVC = UIAlertController(title: "Connected!", message: "The device you selected is now connected", preferredStyle: .alert)
         
         let alertAc = UIAlertAction(title: "ok", style: .default) { (UIAlertAction) in
             
             if let navController = self.navigationController {
-                navController.popViewController(animated: true)
+               navController.popViewController(animated: true)
             }
             
         }
@@ -118,8 +177,17 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         
-        myPeripheral = peripheral
-        BLEDevices.append(peripheral)
+        
+        if(!peripherals.contains(peripheral)) {
+            peripherals.append(peripheral)
+        }
+        
+        
+        
+       //myPeripheral = peripheral
+       //peripherals.append(peripheral)
+        
+        
         
         print("*********************")
         if let namePer = peripheral.name {
@@ -139,6 +207,8 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
     
        
         
+        central.stopScan()
+        self.UITableView0.reloadData()
     }
     
    
@@ -147,7 +217,7 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
         
         names = []
        
-        CBManager?.scanForPeripherals(withServices: [CBUUID.init(string: "DFB0")], options: nil)
+        manager?.scanForPeripherals(withServices: [CBUUID.init(string: "DFB0")], options: nil)
         //CBManager?.stopScan()
         UITableView0.reloadData()
     }
@@ -175,5 +245,13 @@ class BLEsettings: UIViewController, UITableViewDelegate, UITableViewDataSource,
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print(error!)
+    }
+    
+    //
 
 }
--- ENGR679WG/MainPageViewController.swift
+++ ENGR679WG/MainPageViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import Firebase
 import CoreBluetooth
 
-class MainPageViewController: UIViewController{ //, CBCentralManagerDelegate, CBPeripheralDelegate{
+class MainPageViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate{
     
-    /*var manager:CBCentralManager? = nil
+    @IBOutlet weak var recievedMessageText: UILabel!
+    
+    var manager:CBCentralManager? = nil
     var mainPeripheral:CBPeripheral? = nil
     var mainCharacteristic:CBCharacteristic? = nil
     
@@ -21,14 +23,66 @@ class MainPageViewController: UIViewController{ //, CBCentralManagerDelegate, CB
     
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        <#code#>
-    }*/
+        print(central.state)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if (segue.identifier == "scan-segue") {
+            let scanController : BLEsettings = segue.destination as! BLEsettings
+            
+            //set the manager's delegate to the scan view so it can call relevant connection methods
+            manager?.delegate = scanController
+            scanController.manager = manager
+            scanController.parentView = self
+        }
+        
+    }
     
 
+    @IBAction func BLESetting(_ sender: Any) {
+        
+          performSegue(withIdentifier: "scan-segue", sender: self)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ manager = CBCentralManager(delegate: self, queue: nil);
         // Do any additional setup after loading the view.
+        
+        
+        if(mainPeripheral == nil){
+            
+            
+            recievedMessageText.text = "no device is connected"
+            
+        }else{
+            
+            
+            recievedMessageText.text = "the phone is connected to \(String(describing: mainPeripheral))"
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if(mainPeripheral == nil){
+            
+            
+            recievedMessageText.text = "no device is connected"
+            
+        }else{
+            
+            
+            recievedMessageText.text = "the phone is connected to \(String(describing: mainPeripheral))"
+            
+            
+        }
     }
     
     @IBAction func signOutPressed(_ sender: Any) {
@@ -41,16 +95,128 @@ navigationController?.popToRootViewController(animated: true)
             print("there is a problem with signOut")
             
         }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        
 
 }
+    
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        mainPeripheral = nil
+        
+        print("Disconnected" + peripheral.name!)
+    }
+    
+    
+    // MARK: CBPeripheralDelegate Methods
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        
+        for service in peripheral.services! {
+            
+            print("Service found with UUID: " + service.uuid.uuidString)
+            
+            //device information service
+            if (service.uuid.uuidString == "180A") {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+            //GAP (Generic Access Profile) for Device Name
+            // This replaces the deprecated CBUUIDGenericAccessProfileString
+            if (service.uuid.uuidString == "1800") {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+            //Bluno Service
+            if (service.uuid.uuidString == BLEService) {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+        }
+    }
+    
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        //get device name
+        if (service.uuid.uuidString == "1800") {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == "2A00") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found Device Name Characteristic")
+                }
+                
+            }
+            
+        }
+        
+        if (service.uuid.uuidString == "180A") {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == "2A29") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found a Device Manufacturer Name Characteristic")
+                } else if (characteristic.uuid.uuidString == "2A23") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found System ID")
+                }
+                
+            }
+            
+        }
+        
+        if (service.uuid.uuidString == BLEService) {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == BLECharacteristic) {
+                    //we'll save the reference, we need it to write data
+                    mainCharacteristic = characteristic
+                    
+                    //Set Notify is useful to read incoming data async
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    print("Found Bluno Data Characteristic")
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
+        
+        if (characteristic.uuid.uuidString == "2A00") {
+            //value for device name recieved
+            let deviceName = characteristic.value
+            print(deviceName ?? "No Device Name")
+        } else if (characteristic.uuid.uuidString == "2A29") {
+            //value for manufacturer name recieved
+            let manufacturerName = characteristic.value
+            print(manufacturerName ?? "No Manufacturer Name")
+        } else if (characteristic.uuid.uuidString == "2A23") {
+            //value for system ID recieved
+            let systemID = characteristic.value
+            print(systemID ?? "No System ID")
+        } else if (characteristic.uuid.uuidString == BLECharacteristic) {
+            //data recieved
+            if(characteristic.value != nil) {
+                let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
+                
+                recievedMessageText.text = stringValue
+            }
+        }
+        
+        
+    }
+    
+
+    
+    
+    //
 }
